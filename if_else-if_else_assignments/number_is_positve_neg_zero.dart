9333339ebd50ddbc231f6bb17309eb350a626ce9
Rao-Umair-Ali <ruarua682@gmail.com>
2024-07-23 23:44:29 +0500

    Number is positive negative or zero checker

---
+++ if_else-if_else_assignments/number_is_positve_neg_zero.dart
@@ -0,0 +1,13 @@
+void main() {
+//----------------------------------------Question number 1 ------------------------------------------------
+/**  Write a Dart program to check if a given number is positive, negative, or zero.**/
+  print("-----------Question number 1---------");
+  int number10 = 0;
+  if (number10 > 0) {
+    print("&{number10 } is a positive number");
+  } else if (number10 < 0) {
+    print("&{number10 } is a negative number");
+  } else {
+    print("&{number10 } is a neither a positive nor a negative number");
+  }
+}
