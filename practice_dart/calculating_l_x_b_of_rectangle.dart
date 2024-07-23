75380c1e56ed65eac6273d64a0f8749e084b90bc
Rao-Umair-Ali <ruarua682@gmail.com>
2024-07-23 23:36:21 +0500

    Area of rectangle

--- practice_dart/calculating_l_x_b_of_rectangle.dart
+++ practice_dart/calculating_l_x_b_of_rectangle.dart
@@ -2,8 +2,8 @@ void main() {
 //----------------------------------------Question number 3--------------------------------------------------
 
   /* Length and breadth of a rectangle are 5 and 7 respectively. Write a program to calculate the area of the rectangle.*/
-  num length = 5;
-  num breadth = 7;
-  num areaOfRectangle = length * breadth;
+  num length1 = 5;
+  num breadth1 = 7;
+  num areaOfRectangle = length1 * breadth1;
   print("Area of rectangle ${areaOfRectangle} ");
 }
