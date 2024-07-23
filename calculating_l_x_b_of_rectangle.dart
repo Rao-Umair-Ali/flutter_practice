495e9a9fb7351da9061ac887fa9c7143c33486ab
Rao-Umair-Ali <ruarua682@gmail.com>
2024-07-23 23:29:20 +0500

    MArksheet program by print statement

---
+++ calculating_l_x_b_of_rectangle.dart
@@ -0,0 +1,9 @@
+void main() {
+//----------------------------------------Question number 3--------------------------------------------------
+
+  /* Length and breadth of a rectangle are 5 and 7 respectively. Write a program to calculate the area of the rectangle.*/
+  num length = 5;
+  num breadth = 7;
+  num areaOfRectangle = length * breadth;
+  print("Area of rectangle ${areaOfRectangle} ");
+}
