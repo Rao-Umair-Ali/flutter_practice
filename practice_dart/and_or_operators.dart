fc718d1eb63af92517ba3d7ab35c99ccfe6afb25
Rao-Umair-Ali <ruarua682@gmail.com>
2024-07-23 23:33:28 +0500

    And Or Operators

---
+++ practice_dart/and_or_operators.dart
@@ -0,0 +1,10 @@
+void main() {
+  //----------------------------------------Question number 5--------------------------------------------------
+
+/*Write a program create two integer variables 'a' and 'b' and assign them any number and then check if both the conditions 
+'a < 50' and 'a < b' are true. Display the result, now check if at-least one of the conditions 'a < 50' and 'a < b' is true.*/
+  int a = 13;
+  int b = 7;
+  print(a < 50 && a < b);
+  print(a < 50 || a < b);
+}
