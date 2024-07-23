8948fa43e1f204a4a493ec42e8bdf6d6d5f765da
Rao-Umair-Ali <ruarua682@gmail.com>
2024-07-24 00:00:04 +0500

    Practice of list

---
+++ List/practice_list.dart
@@ -0,0 +1,7 @@
+void main() {
+  List studentsName = ["umair", "hammad", "zunair"];
+
+  print(" this give me whole list ${studentsName}");
+  print(" this give me 1st index value from list ${studentsName[1]}");
+  print(" this give me whole list length  ${studentsName.length}");
+}
