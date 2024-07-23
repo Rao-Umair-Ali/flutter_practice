49bdaa7b3b1fd75aaa8d86462e8b5c1c2a9f4626
Rao-Umair-Ali <ruarua682@gmail.com>
2024-07-23 23:46:22 +0500

    Number is even or odd

---
+++ if_else-if_else_assignments/number_even_odd.dart
@@ -0,0 +1,12 @@
+void main() {
+/**Write a Dart program to check if a given number is even or odd.
+**/
+  print("-------Question number 2---------");
+  int numbers = 0;
+
+  if ((numbers % 2) == 0) {
+    print("&{numbers} is a even number");
+  } else {
+    print("&{numbers} is an odd number");
+  }
+}
