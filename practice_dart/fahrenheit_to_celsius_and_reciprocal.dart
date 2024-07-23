9fccdb449a1318a41ede41489517dc27c3d5d22f
Rao-Umair-Ali <ruarua682@gmail.com>
2024-07-23 23:35:03 +0500

    fahrenheit to celsius and its reciprocal

--- practice_dart/fahrenheit_to_celsius_and_reciprocal.dart
+++ practice_dart/fahrenheit_to_celsius_and_reciprocal.dart
@@ -5,9 +5,9 @@ void main() {
 °F = (Temperature in Celsius (°C) * 9/5) + 32
 °C = (Temperature in Fahrenheit (°F) - 32) * 5/9*/
 //conversion of celsius to fahrenhiet.
-  double celsius = 245;
-  double fahrenheit = (celsius * (9 / 5)) + 32;
-  print("your temperature in fahrenheit is ${fahrenheit} fahrenheit");
+  double celsius1 = 245;
+  double fahrenheit1 = (celsius1 * (9 / 5)) + 32;
+  print("your temperature in fahrenheit is ${fahrenheit1} fahrenheit");
 // conversion of fahreheit to celsius .
   double fahrenheit2 = 245;
   double celsius2 = (fahrenheit2 - 32) * (5 / 9);
