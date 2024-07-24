8aeefc226ee7a79e504181b3c7ea78df8266f796
Rao-Umair-Ali <ruarua682@gmail.com>
2024-07-24 23:19:33 +0500

    Creating  a list of information

---
+++ List/List_of_information.dart
@@ -0,0 +1,5 @@
+void main() {
+  //create a list  of name, class, roll no, grade, percentage. And print.
+  List information = ["Rao Umair Ali", "2nd year ", "113", "A", "71.68 % "];
+  print(information);
+}
