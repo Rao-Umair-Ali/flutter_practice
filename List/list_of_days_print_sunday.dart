fdb252cd4b7ed6b1d3bc30834227cbdb01e8471e
Rao-Umair-Ali <ruarua682@gmail.com>
2024-07-24 23:16:27 +0500

    Creating  a list of Days and print only  Sunday

---
+++ List/list_of_days_print_sunday.dart
@@ -0,0 +1,14 @@
+void main() {
+//Q. 2) Create a list of Days and print only  Sunday
+
+  List<String> daysName = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "saturday",
+    "Sunday"
+  ];
+  print(daysName[daysName.length - 1]);
+}
