cfb87319be642748117615f71fa7b59bd65c3f84
Rao-Umair-Ali <ruarua682@gmail.com>
2024-08-15 18:02:06 +0500

    nested Map

---
+++ MapListFunctionLoopQuestions/nestedMap.dart
@@ -0,0 +1,22 @@
+void main() {
+  Map countries = {
+    "Pakistan": {
+      "capitalCity": "Islamabad",
+      "currency": "pkr",
+      "language": "Urdu"
+    },
+    "India": {"capitalCity": "Delhi", "currency": "Ir", "language": "Hindi"},
+    "Afghanistan": {
+      "capitalCity": "Kabul",
+      "currency": "Ar",
+      "language": "Pashtoon"
+    },
+    "China": {
+      "capitalCity": "Beijing",
+      "currency": "chinarupees",
+      "language": "chinese"
+    }
+  };
+
+  print(countries["Pakistan"]);
+}
