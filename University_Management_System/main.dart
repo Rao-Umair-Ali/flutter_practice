edc4980a65fe5a06063c697bcfcaa7d79fd51b2a
Rao-Umair-Ali <ruarua682@gmail.com>
2024-08-25 12:14:27 +0500

    register and login

--- University_Management_System/main.dart
+++ University_Management_System/main.dart
@@ -1,39 +1,19 @@
+import 'userRegister.dart';
 import 'dart:io';
 
 main() {
-  print("Welcome to the School");
-  print("Registeration form ");
-  Map data = {};
-  print("Enter Roll number");
-  String? Key = stdin.readLineSync();
-  print("Enter Your Full name ");
-  String? fullName = stdin.readLineSync();
-  print("Enter Email");
-  String? email = stdin.readLineSync();
-  print("Enter Password ");
-  String? password = stdin.readLineSync();
-  Map dataPerson = {"fullName": fullName, "email": email, "password": password};
-  data[Key] = dataPerson;
-  print("You succesfullly createrd your account ");
-  print("Login to continue ");
-  print("Enter Email");
-  String? userEmail = stdin.readLineSync();
-  print("Enter Password");
-  String? userPassword = stdin.readLineSync();
-  if (userEmail == dataPerson["email"] &&
-      userPassword == dataPerson["password"]) {
-    print("Succesfullly Login");
-    print(dataPerson);
-    print(
-        "if you want to change your password type 'p' \n if you want to see pending fees press 'p' \n if you want to pay fees press 'c' \n if you want delete your account press d ");
+  print("Welcome to the university");
+  String? input = "";
+  Register newone = Register();
+  while (input != "6") {
+    print("1 : for register");
+    print("2: Login ");
     String? input = stdin.readLineSync();
-    if (input == 'p' || input == 'P') {
-      print("enter you new password");
-      String? pass = stdin.readLineSync();
-      dataPerson["password"] = pass;
-      print(dataPerson);
+    if (input == "1") {
+      newone.information();
     }
-  } else {
-    print("you give me wrong credential");
+    if (input == "2") {
+      newone.login();
+    }
   }
 }
---
+++ University_Management_System/userRegister.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+
+class Register {
+  List info = [];
+  Map details = {};
+
+  void information() {
+    print("give me id number");
+
+    print("Give me your user name:");
+    String? name = stdin.readLineSync();
+    details["name"] = name;
+
+    print("Give me your email:");
+    String? email = stdin.readLineSync();
+    details["email"] = email;
+
+    print("Give me your password:");
+    String? password = stdin.readLineSync();
+    details["password"] = password;
+    info.add(details);
+
+    print(info);
+
+    print("User successfully created.");
+  }
+
+  void login() {
+    print("Login:");
+    print("Give me your email:");
+    String? useremail = stdin.readLineSync();
+
+    print("Give me your password:");
+    String? userpassword = stdin.readLineSync();
+
+    if (useremail == details["email"] && userpassword == details["password"]) {
+      print("User successfully logged in.");
+    } else {
+      print("Kindly provide correct credentials.");
+    }
+  }
+}
