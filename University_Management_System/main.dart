0364481874fc24b5142a4ca2bbc0c473cd569e13
Rao-Umair-Ali <ruarua682@gmail.com>
2024-08-25 12:38:37 +0500

    admin page

--- University_Management_System/main.dart
+++ University_Management_System/main.dart
@@ -1,3 +1,4 @@
+import 'admin.dart';
 import 'userRegister.dart';
 import 'dart:io';
 
@@ -5,15 +6,24 @@ main() {
   print("Welcome to the university");
   String? input = "";
   Register newone = Register();
+  Admin newAdmin = Admin();
+
   while (input != "6") {
-    print("1 : for register");
-    print("2: Login ");
+    print("1 : Admin");
+    print("2: Student");
     String? input = stdin.readLineSync();
     if (input == "1") {
-      newone.information();
-    }
-    if (input == "2") {
-      newone.login();
+      if (input == "1") {
+        newAdmin.information();
+      } else if (input == "2") {
+        newAdmin.login();
+      }
+    } else if (input == "2") {
+      if (input == "1") {
+        newone.information();
+      } else if (input == "2") {
+        newone.login();
+      }
     }
   }
 }
